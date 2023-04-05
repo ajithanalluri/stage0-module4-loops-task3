ca776bf8835f4fd6f5094336706f53acdc9d4fa4
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-05 11:52:16 +0530

    #1

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0;
+        for(int i=t; i!=0; i=i/10) {
+            sum = sum+i%10;
+        }
+        if(t > 0){
+            System.out.println(sum);
+        } else{
+            System.out.println(Math.abs(sum));
+        }
     }
 }
